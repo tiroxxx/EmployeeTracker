6b0aa15fb4f30735df6474dc60bb559fe836b21c
tiroxxx <christianmarquez41@gmail.com>
2020-10-11 21:50:55 -0400

    added all prompts for adding an employee

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const logo = require("asciiart-logo");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Christian41!",
+    database: "employeetracker_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    init();
+});
 
 function init() {
     // prints a cool ascii art
@@ -24,10 +37,10 @@ function startPrompts() {
             choices: [
                 "Add Employee", "Add Department", "Add Role", "Update Employee Role", "View Deparments", "View Employees", "View Role"
             ]
-        }).then( answer => {
+        }).then(answer => {
             switch (answer.decision) {
                 case "Add Employee":
-                    addEmplyee();
+                    addEmployee();
                     break;
                 case "Add Department":
                     addDepartment();
@@ -49,4 +62,73 @@ function startPrompts() {
                     break;
             }
         })
+}
+
+function addEmployee() {
+    console.log("add employee");
+
+    inquirer.prompt(
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is the employee's first name?",
+            validate: answer => {
+                // checking for empty input
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter a valid name";
+            }
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employee's last name?",
+            validate: answer => {
+                // checking for empty input
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter a valid name";
+            }
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What is the employee's role?",
+            choices: [
+
+            ]
+        },
+        {
+            type: "list",
+            name: "manager",
+            message: "Who is the employee's manager?",
+            choices: []
+        }
+    ).then({
+        
+        startPrompts();
+    })
+
+    connection.query("INSERT INTO auctions SET ?")
+}
+
+function addDepartment() {
+    console.log(("add department"));
+}
+function addRole() {
+    console.log(("add role"));
+}
+function updateEmployeeRole() {
+    console.log(("update employee role"));
+}
+function viewDepartments() {
+    console.log(("view department"));
+}
+function viewEmployees() {
+    console.log(("view employees"));
+}
+function viewRole() {
+    console.log(("view Role"));
 }
